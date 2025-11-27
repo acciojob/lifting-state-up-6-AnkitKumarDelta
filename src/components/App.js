2d62eeaf9594db15068f8428ef61cb76f67199be
AnkitKumarDelta <ankitna355@gmail.com>
2025-11-27 21:24:54 +0530

    save commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,21 @@ import './../styles/App.css';
 import TodoList from "./TodoList";
 
 const App = () => {
-  const[todos,setTodos] = useState([
-    {id:1,task:"Learn React",iscomplete:false},
-    {id:2,task:"Build a React app", iscomplete:false},
-    {id:3,task:"Deploy the React app",iscomplete:false}
+  const [todos, setTodos] = useState([
+    {id: 1, task: "Learn React", isComplete: false},
+    {id: 2, task: "Build a React app", isComplete: false},
+    {id: 3, task: "Deploy the React app", isComplete: false}
   ]);
 
-  function handleComplete(id){
-    let updatedTodo = todos.map(t=>t.id===id?{...t,iscomplete:true}:t);
+  function handleComplete(id) {
+    let updatedTodo = todos.map(t => t.id === id ? {...t, isComplete: true} : t);
     setTodos(updatedTodo);
   }
   
   return (
     <div>
       <h1>Parent Component</h1>
-      <TodoList todos={todos} onClick={handleComplete}/>
+      <TodoList todos={todos} handleComplete={handleComplete}/>
     </div>
   )
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,19 @@
 import React from "react";
-export default function TodoList({todos, onClick}){
-    return(
+
+export default function TodoList({todos, handleComplete}) {
+    return (
         <div>
             <h2>Child Component</h2>
             <ul>
-        {todos.map(t=>{
-            return(
-                <li key={t.id}>
-                    <span>{t.task}</span>
-                    {!t.iscomplete ? <button onClick={()=>onClick(t.id)}>Complete</button> : null}
-                </li>
-            )
-        })}
-        </ul>
+                {todos.map(t => {
+                    return (
+                        <li key={t.id}>
+                            <span>{t.task}</span>
+                            {!t.isComplete ? <button onClick={() => handleComplete(t.id)}>Complete</button> : null}
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
