bc3c7f7278551d4c5d820a19c4fc28fa156d18f0
AnkitKumarDelta <ankitna355@gmail.com>
2025-11-27 21:18:48 +0530

    save final

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,23 @@
-
 import React, { useState } from "react";
 import './../styles/App.css';
 import TodoList from "./TodoList";
 
 const App = () => {
   const[todos,setTodos] = useState([
-    {id:1,task:"Learn React",iscomplete:false,},{id:2,task:"Build a React app", iscomplete:false,} , {id:3,task:"Deploy the React app",iscomplete:false,}
+    {id:1,task:"Learn React",iscomplete:false},
+    {id:2,task:"Build a React app", iscomplete:false},
+    {id:3,task:"Deploy the React app",iscomplete:false}
   ]);
 
   function handleComplete(id){
-let updatedTodo = todos.map(t=>t.id===id?{...t,iscomplete:true}:t);
-setTodos(updatedTodo);
+    let updatedTodo = todos.map(t=>t.id===id?{...t,iscomplete:true}:t);
+    setTodos(updatedTodo);
   }
+  
   return (
     <div>
       <h1>Parent Component</h1>
-        <TodoList todos={todos} onClick={handleComplete}/>
+      <TodoList todos={todos} onClick={handleComplete}/>
     </div>
   )
 }
